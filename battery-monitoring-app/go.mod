0474bfd91a53af8ea89749324fe312ef7c9d3aed
Kolade Bashir <patrickbashiru01@gmail.com>
2024-12-14 23:32:29 +0100

    Create initial structure of app with main.go

---
+++ battery-monitoring-app/go.mod
@@ -0,0 +1,3 @@
+module battery-monitoring-app
+
+go 1.22.5
---
+++ battery-monitoring-app/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"time"
+	"os"
+	"strings"
+)
+
+func GetBatteryPercentage() string {
+	// pmset is a terminal command for power management
+	cmd := exec.Command("pmset", "-g", "batt")
+
+	// Ouput() returns the output of the command as a slice of bytes
+	output, err := cmd.Output()
+	if err != nil {
+		fmt.Println("Command exited with error:", err)
+	}
+	o := string(output)
+
+
+	s := strings.Split(o, ";")
+	bp := s[0]
+
+	str := strings.Split(bp, ")")[1]
+	return strings.Split(strings.TrimSpace(str), "%")[0]
+	
+}
+
+func main() {
+
+	if len(os.Args) != 2 {
+		fmt.Println("Insufficent number of arguments provided")
+	}
+
+	// Takes the users specified battery level from the argument provided when the script runs
+	userBatteryLevel := os.Args[1]
+
+	for {
+		batteryPercent := GetBatteryPercentage()
+		if batteryPercent == userBatteryLevel {
+
+			// osascript is used to execute Javascript and Applescript commands
+			s := fmt.Sprintf(`display notification "Battery Level reached %v" with title "Battery Notification"`, batteryPercent)
+			cmd2 := exec.Command("osascript", "-e", s)
+			cmd2.Output()
+		}
+		time.Sleep(2 * time.Second)
+
+	}
+}
